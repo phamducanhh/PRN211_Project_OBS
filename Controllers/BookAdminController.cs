0a6b8f73fa8679586e077ae72cb8c5b1f53e68e4
hoahv20 <hoahv2000@gmail.com>
2021-10-28 21:28:33 +0700

    Test

---
+++ Controllers/BookAdminController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PRN211_Project_OBS.Controllers
+{
+    public class BookAdminController : Controller
+    {
+        // GET: BookAdmin
+        public ActionResult BookAdmin()
+        {
+            ViewBag.Welcome = "welcome to BookAdmin";
+            return View();
+        }
+    }
+}
