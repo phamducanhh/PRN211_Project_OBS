65f4934cea721537c27d7c4f57f4788300a02373
hoahv20 <hoahv2000@gmail.com>
2021-11-08 02:10:40 +0700

    Finish AddBook function

--- Controllers/AdminHomeController.cs
+++ Controllers/AdminHomeController.cs
@@ -10,6 +10,7 @@ namespace PRN211_Project_OBS.Controllers
     public class AdminHomeController : Controller
     {
         DAO dao = new DAO();
+        Model db = new Model();
         //List<Book> books = new List<Book>();
         // GET: AdminHome
         public ActionResult Index()
@@ -21,13 +22,29 @@ namespace PRN211_Project_OBS.Controllers
         public ActionResult AddBook()
         {
             ViewBag.GenreList = dao.GetGenres();
+            ViewBag.AuthorList = dao.GetAllAuthors();
             return View();
         }
 
         [HttpPost]
-        public ActionResult AddBook(string title, string imageUrl, string author, int[] genreId, string description, float price)
+        public ActionResult AddBook(string title, string imageUrl, int author, int[] genre, string description, float price)
         {
-
+            Book book = new Book
+            {
+                title = title,
+                image_url = imageUrl,
+                author_id = author,
+                description = description,
+                price = price
+            };
+            db.Books.Add(book);
+            db.SaveChanges();
+            int bookId = dao.GetBookByTitle(title).id;
+            for(int i = 0; i < genre.Length; i++)
+            {
+                dao.InsertGenreBook(bookId, genre[i]);
+            }
+            db.SaveChanges();
             return RedirectToAction("Index");
         }
     }
