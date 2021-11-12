64416e802e545edd08007a640eb18b3b8f4f5048
hoahv20 <hoahv2000@gmail.com>
2021-11-13 00:41:27 +0700

    Listed Author

--- Final_PRN211_OBS_Project/Controllers/AuthorAdminController.cs
+++ Final_PRN211_OBS_Project/Controllers/AuthorAdminController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Final_PRN211_OBS_Project.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +9,12 @@ namespace Final_PRN211_OBS_Project.Controllers
 {
     public class AuthorAdminController : Controller
     {
+        DAO dao = new DAO();
+        Model db = new Model();
         // GET: AuthorAdmin
         public ActionResult Index()
         {
+            ViewBag.AuthorList = dao.GetAllAuthors();
             return View();
         }
     }
